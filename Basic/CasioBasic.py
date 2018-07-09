db830f0f9a947a5670ae78b2c4a7b472eab7586e
GreenPIsoftware <vjvsp@yahoo.de>
2018-07-09 15:45:59 +0200

    p

--- Basic/CasioBasic.py
+++ Basic/CasioBasic.py
@@ -1,32 +1,74 @@
-
 def exec():
-    return 'Ù'
+    return 'Ù\n'
+
 
 def pr(txt):
     return '"' + txt + '"'
 
+
 def assign(var, val):
-    return val.upper() + "ã" + var.upper()
+    return str(val).upper() + "ã" + str(var).upper()
+
 
 def rd(var):
     return assign(var, "?")
 
-def l(*words):
-    return ' '.join(words)
 
-def main(*lines):
-    return (exec() + '\n').join(lines) + exec()
+def main(*lines, globals=[]):
+    return (exec()).join(globals) + exec() + (exec()).join(lines) + exec()
+
+
+def _if(cond, then, _else):
+    cmd = "If " + cond + exec() + then + exec()
+    if _else is not None:
+        cmd += "\nElse " + _else + exec()
+    return cmd
+
+
+def _for(var, _from, to, step, *lines):
+    return "For " + assign(var, _from) + " to " + str(to) + " Step " + str(step) + exec() + \
+           exec().join([str(x) for x in lines]) + exec() + "Next" + exec()
+
+
+def _while(condition, *code):
+    return "While " + condition + exec() + exec().join([str(x) for x in code]) + exec() + "WhileEnd" + exec()
+
+
+def list(i, j):
+    return " List" + str(i) + "[" + str(i) + "] "
+
+class List([]):
+
+    @staticmethod
+    def alloc(var, length):
+        return assign(var, " DimList " + length)
+
+    @staticmethod
+    def get(var, i):
+        return var + "[" + i + "]"
+
 
 
 if __name__ == '__main__':
     prgrm = main(
         pr("hello user!"),
         rd("A"),
-        assign("B", "55"),
+        assign("B", 51),
         assign("C", "A+B"),
-        "C"
+        "C",
+        _for(
+            "A", 1, 100, 1,
+            pr("A")
+        ),
+        assign("X", list())
+        _while(
+            "A=1",
+            assign("A", "A+1"),
+            pr("A")
+
+        )
     )
-    print(prgrm.upper())
+    print(prgrm)
 
     with open("Output.txt", "w") as text_file:
         text_file.write(prgrm)
