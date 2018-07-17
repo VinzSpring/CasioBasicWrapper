10309a428c13022a9b7e3249579cb0e0e49577be
IamTheOneWhoKnocks <vjvsp@yahoo.de>
2018-07-17 14:58:31 +0200

    refactoring, adding Ht

--- Basic/CasioBasic.py
+++ Basic/CasioBasic.py
@@ -29,12 +29,12 @@ def _if(cond, then, _else):
     return cmd + exec() + "IfEnd"
 
 
-def _for(var, _from, to, step, *lines):
+def cb_for(var, _from, to, step, *lines):
     return "For " + assign(var, _from) + " To " + str(to) + " Step " + str(step) + exec() + \
            exec().join([str(x) for x in lines]) + exec() + "Next"
 
 
-def _while(condition, *code):
+def cb_while(condition, *code):
     return "While " + condition + exec() + exec().join([str(x) for x in code]) + exec() + "WhileEnd"
 
 
@@ -53,99 +53,22 @@ def alloc_list(list_num, length):
 def alloc_matrix(mat_name, i, j):
     return assign("Dim Mat " + mat_name, "{" + str(i) + "," + str(j) + "}")
 
-def mult(a, b):
-    return str(a) + "À" + str(b)
+
+def mult(*a):
+    return "À".join(a)
+
 
 def clear(var):
     return assign(var, 0)
 
+
 def logab(a, b):
     return "log a(b)" + str(a) + "," + str(b) + ")"
 
-def sortA(list):
-    return "SortA(" + str(list) + ")"
 
-if __name__ == '__main__':
-    global_vars = [
-        assign("D", 10),
-    ]
-
-    prgrm = main(
-        pr_txt("hello user!"),
-        pr_txt("global vars:"),
-        pr_txt("D:"), pr_var("D"),
-
-        alloc_matrix("A", 2, 2),
-        pr_var("Mat A"),
-
-        alloc_list(1, 10),
-        pr_var("List 1"),
-
-        rd("A"),
-        assign("B", 51),
-        assign("C", "A+B"),
-        pr_var("C"),
-        _for(
-            "A", 1, 30, 1,
-            pr_var("A")
-        ),
-        assign("A", 1),
-        _while(
-            "A<10",
-            assign("A", "A+1"),
-            pr_var("A")
-        ),
-
-        globals=global_vars
-    )
-
-    entropy = main(
-        pr_txt("Entropy"),
-
-        # pr_txt("sub intervals:"),
-        # rd("C"),
-        pr_txt("N:"),
-        rd("N"),
-        #sum
-        clear("A"),
-        clear("B"),
-        _for(
-            "I", 0, "N-1", 1,
-            pr_txt("enter p(i):"),
-            rd("A"),
-            assign("B", "B+" + mult("A", logab(2, div(1, "A"))))
-        ),
-
-        pr_var("B"),
-    )
-
-    #print(entropy)
+def sortA(ls):
+    return "SortA(" + str(ls) + ")"
 
 
-
-    global_shanon_fano = [  
-        alloc_list(1, "N"),
-        alloc_matrix("M", "N", 255),
-    ]
-    shannon_fano = main(
-        pr_txt("Shannon Fano"),
-
-        # number of codewords
-        pr_txt("N:"),
-        rd("N"),
-
-        # read codeword probability
-        _for("I", 1, "N", 1,
-            pr_txt("enter p(i):"),
-            rd("List 1[I]"),
-        ),
-
-        # sort ascending
-        sortA("List 1"),
-
-        pr_var("List 1"),
-
-        globals=global_shanon_fano
-    )
-
-    print(entropy)
+def add(*a):
+    return "+".join(a)
